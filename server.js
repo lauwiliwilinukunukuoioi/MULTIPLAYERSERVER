89c1ba46ad3a9b9e101925694f7a9c7441a1b749
lauwiliwilinukunukuʻoiʻoi <97569855+lauwiliwilinukunukuoioi@users.noreply.github.com>
2025-02-26 22:08:13 -0500

    fixed a bunch of stuff :D

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 var dgram = require("dgram");
 
 var server = dgram.createSocket("udp4");
-var server2 = dgram.createSocket("udp4");
 
 var data;
-var datahere = false;
+
+const msgType = {
+
+    CREATE_HOST: 0,
+	JOIN_HOST: 1,
+	STOP_HOST: 2,
+    SET_PLAYER_STAT: 3
+
+}
 
 server.on("message", function(msg, rinfo){
-    console.log(String(msg))
+    console.log("< " + String(msg))
     data = JSON.parse(msg);
-    datahere = true;
-});
-
-server2.on("message", function(msg, rinfo){
-    if (datahere) {
-    server2.send(JSON.stringify(data), rinfo.port, rinfo.address);
+    switch (data.type) {
+        case msgType.SET_PLAYER_STAT:
+            set_player_stat(data,rinfo);
+            break;
+    
+        default:
+            break;
     }
 });
 
+function set_player_stat(data,rinfo){
+    console.log("set_player_stat function")
+
+    server.send(JSON.stringify(data), rinfo.port, rinfo.address)
+}
+
 server.bind(8080);
-server2.bind(8081);
