20ea31d7290f8c6973172cf0f6d441fe7e4fcb27
lauwiliwilinukunukuʻoiʻoi <97569855+lauwiliwilinukunukuoioi@users.noreply.github.com>
2025-02-26 21:36:46 -0500

    Client & Host Server (will remove later)

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var dgram = require("dgram");
 
 var server = dgram.createSocket("udp4");
+var server2 = dgram.createSocket("udp4");
 
 var data;
+var datahere = false;
 
 server.on("message", function(msg, rinfo){
-    console.log(String(msg));
+    console.log(String(msg))
     data = JSON.parse(msg);
-    console.log(String(data.x));
-    //server.send("message has recieved", rinfo.port, rinfo.adress);
+    datahere = true;
 });
 
-server.bind(8080)
+server2.on("message", function(msg, rinfo){
+    if (datahere) {
+    server2.send(JSON.stringify(data), rinfo.port, rinfo.address);
+    }
+});
+
+server.bind(8080);
+server2.bind(8081);
