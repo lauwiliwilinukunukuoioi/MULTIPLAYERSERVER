780f2b76190014368b3fa43af0758709afad6659
lauwiliwilinukunukuʻoiʻoi <97569855+lauwiliwilinukunukuoioi@users.noreply.github.com>
2025-02-27 22:32:44 -0500

    server start and stops

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ var dgram = require("dgram");
 var server = dgram.createSocket("udp4");
 
 var data;
-
+var hosts = [];
 const msgType = {
 
     CREATE_HOST: 0,
 	JOIN_HOST: 1,
 	STOP_HOST: 2,
     SET_PLAYER_STAT: 3
+
+}
+
+function player(x,y){
+    this.x = x;
+    this.y = y;
 
 }
 
@@ -20,6 +26,13 @@ server.on("message", function(msg, rinfo){
         case msgType.SET_PLAYER_STAT:
             set_player_stat(data,rinfo);
             break;
+        case msgType.CREATE_HOST:
+            create_host(data,rinfo);
+            break;
+        case msgType.STOP_HOST:
+            stop_host(data,rinfo);
+            break;
+        
     
         default:
             break;
@@ -29,7 +42,29 @@ server.on("message", function(msg, rinfo){
 function set_player_stat(data,rinfo){
     console.log("set_player_stat function")
 
-    server.send(JSON.stringify(data), rinfo.port, rinfo.address)
+    
+}
+
+function create_host(data,rinfo){
+    console.log("create_host function")
+    var hostNumber = hosts.length;
+    hosts.push([new player(0,0)]);
+
+    data.hostNumber = hostNumber;
+    data.playerNumber = 0;
+
+    server.send(JSON.stringify(data), rinfo.port, rinfo.address);
+    console.table(hosts);
+
+}
+
+function stop_host(data, rinfo){
+    console.log("Stop_host function");
+    var host_to_stop = hosts.indexOf(data.hostnumber);
+    hosts.splice(host_to_stop, 1);
+    data.res = "stoped";
+    server.send(JSON.stringify(data), rinfo.port, rinfo.address);
+    console.table(hosts);
 }
 
 server.bind(8080);
